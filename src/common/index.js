6e69d308b6dbfac309499925eb40d48097ebc8a7
zhuzesen <m15626248624@163.com>
2019-07-24 14:34:03 +0800

    祝泽森更新按钮组件

--- src/common/index.js
+++ src/common/index.js
@@ -1,17 +1,118 @@
+import 'core-js'
 import React from 'react';
-import imgTable from './images/table/table.png';
-import imgPage from './images/pagenation/page.png';
-const Table = function () {
-  console.log("this is a common plugin Table!");
-  return <div className='table'><img src={imgTable}></img></div>
-};
+import './index.scss'
 
-const PageNation = function () {
-    console.log("this is a common plugin PageNation!");
-    return <div className='page'><img src={imgPage}></img></div>
-};
+/*
+* 组件：按钮组
+* params
+*style:样式
+* func:方法
+* details:内容
+* type:按钮颜色类型
+* size:按钮大小
+* */
+class Button extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            style : props.style,
+            func : props.func,
+            details : props.details,
+            type: props.type,
+            size: props.size,
+            mouseEnter:false
+        }
+    }
 
+
+
+    /*
+    * type类型选择
+    * */
+
+    TypeSelect = () => {
+        let ButtonType = 'button_type';
+        console.log(this.state.type)
+        switch(this.state.type)
+        {
+            case 'normal':
+                ButtonType =  'button_type_normal'
+               break;
+            case 'primary':
+                ButtonType = 'button_type_primary'
+                break;
+            default:
+                console.log('Button属性赋值不合法')
+
+        }
+        this.setState({ButtonType:ButtonType})
+    }
+
+    /*
+     * size类型选择
+     * */
+
+    SizeSelect = () => {
+        let ButtonSize = 'button_size';
+        console.log(this.state.size)
+        switch(this.state.size)
+        {
+            case 'small':
+                ButtonSize =  'button_size_small'
+                break;
+            case 'normal':
+                ButtonSize = 'button_size_normal'
+                break;
+            default:
+                console.log('Button属性赋值不合法')
+
+        }
+        this.setState({ButtonSize:ButtonSize})
+    }
+
+    /*
+    * 鼠标移入
+    * */
+    MouseEnter = (event) => {
+        /*$(event.target).addClass('Button_mouse_enter')*/
+        this.setState({mouseEnter:true})
+        console.log(this.state.mouseEnter)
+    }
+    /*
+     * 鼠标移出
+     * */
+    MouseLeave = (event) => {
+        this.setState({mouseEnter:false})
+        console.log(this.state.mouseEnter)
+    }
+
+    componentDidMount(){
+        this.TypeSelect();
+        this.SizeSelect();
+    }
+
+    render(){
+        return (
+            <input
+                type="button"
+                className={`ButtonBase ${this.state.ButtonType} ${this.state.ButtonSize} ${this.state.mouseEnter?'Button_mouse_enter':''}`}
+
+                style={this.state.style}
+                onClick={this.state.func}
+                value = {this.state.details}/>
+
+
+        )
+    }
+}
+
+/*
+ * 按钮组件默认属性
+ * */
+Button.defaultProps = {
+    /*type:'normal',
+    size:'normal'*/
+}
 export {
-    PageNation,
-    Table
-};
+   Button
+}
