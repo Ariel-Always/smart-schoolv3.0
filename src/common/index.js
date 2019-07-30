57209f7994cf0c1e38bb7740650c99e8911eb6b5
Ariel-Always <zhangyangllx@163.com>
2019-07-30 21:09:48 +0800

    上传搜索组件

--- src/common/index.js
+++ src/common/index.js
@@ -174,14 +174,103 @@ Button.defaultProps = {
  * */
     class PagiNation extends React.Component{
         render() {
-            const {children,hideOnSinglePage,...reset} = this.props;
+            const {children,hideOnSinglePage,simple,showQuickJumper,...reset} = this.props;
             return (
-                <AntPagination {...reset} hideOnSinglePage={hideOnSinglePage?hideOnSinglePage:true}>{children}</AntPagination>
+                <AntPagination {...reset} hideOnSinglePage={hideOnSinglePage?hideOnSinglePage:true}
+                               showQuickJumper={simple?false:{goButton: <span className="pagination_go_button">Go</span>}}
+                simple={simple?true:false}
+                >{children}</AntPagination>
             );
         }
     }
 /*
  * 分页组件 end
+ * */
+/*
+ * 搜索 start
+ * */
+    class Search extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state={
+                selectShow:false,
+                cancleShow:false
+            }
+        }
+        showSelect(e){
+            this.setState({selectShow:!this.state.selectShow});
+        }
+        changeSelect(e){
+            this.setState({selectdValue:{value:e.value,title:e.title}});
+            this.setState({selectShow:!this.state.selectShow});
+        }
+        render() {
+            const {width,select,placeHolder,selectOptions,onClickSearch}= this.props;
+            return (
+                <div className="search_container" style={{width:width?width:''}}>
+                    <table className="search_wrapper_table">
+                        <tbody>
+                        <tr>
+                            {//控制下拉部分的宽度
+                                select? <td style={{width:(selectOptions&&selectOptions.width)?selectOptions.width:'86px'}}>
+                                <div className="search_select_wrapper">
+                                        <span className="search_select_span" onClick={this.showSelect.bind(this)}
+                                              style={{width:(selectOptions&&selectOptions.width)?(selectOptions.width-14):''}}>
+                                            <span className={`search_select_icon ${this.state.selectShow===true?'search_slide_icon':''}`}></span>
+                                            {//默认的选项(是否state有值)？（设置为state的值）:(是否外界传值)？：使用外界值：默认值（搜学生）
+                                                this.state.selectdValue?
+                                                <span className="search_select_text" data-value={this.state.selectdValue.value} title={this.state.selectdValue.title}>{this.state.selectdValue.title}</span>
+                                                :
+                                                ((selectOptions&&selectOptions.default)?
+                                                <span className="search_select_text" data-value={selectOptions.default.value} title={selectOptions.default.title}>{selectOptions.default.title}</span>
+                                                :
+                                                <span className="search_select_text">搜学生</span>
+                                                )
+                                            }
+
+                                        </span>
+                                    <ul className="search_select_ul"
+                                    style={{display:this.state.selectShow===true?'block':'none'}}>
+                                        {
+                                            //选项列表 (是否外界传值)？：使用外界值：默认值（搜学生）
+                                            (selectOptions&&selectOptions.selectArray)?
+                                             selectOptions.selectArray.map((item,k)=>{
+                                                return <li key={k} onClick={this.changeSelect.bind(this,{value:item.value,title:item.title})} className="search_select_li" data-value={item.value} title={item.title}>{item.title}</li>
+                                             })
+                                             :
+                                             <li className="search_select_li">搜学生</li>
+                                        }
+
+                                    </ul>
+                                    <span className="search_select_gap"></span>
+                                </div>
+                            </td>:''}
+                            <td className="search_left_td">
+                                <input id="search_text_input" className="search_text_input" type="text" placeholder={placeHolder} />
+                                <input className="search_cancel_input" type="button" style={{display:this.state.cancelShow===true?'block':'none'}} />
+                            </td>
+                            <td className="search_right_td">
+                                <input  className="search_btn_input" type="button"
+                                       onClick={
+                                           ()=>onClickSearch(
+                                               {selectValue:
+                                               this.state.selectdValue?this.state.selectdValue.value
+                                               :selectOptions.default.value,
+                                               value:document.getElementById("search_text_input").value
+                                               }
+                                               )
+                                       }
+                                />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            );
+        }
+    }
+/*
+ * 搜索 end
  * */
 export {
    Button,
@@ -190,5 +279,6 @@ export {
     CheckBox,
     CheckBoxGroup,
     Table,
-    PagiNation
+    PagiNation,
+    Search
 }
