38b3d7d5fc51e89fbb46b8a41087b8a4ac853bba
zhuzesen <m15626248624@163.com>
2019-08-07 11:40:55 +0800

    19/8/7

--- src/index/index.js
+++ src/index/index.js
@@ -5,14 +5,11 @@ import ReactDOM from 'react-dom';
 import './css/index.scss';
 /*import '../common/index.scss'*/
 import App from './js/containers/App';
-import {LocaleProvider} from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
+
 
 import * as serviceWorker from '../serviceWorker';
-moment.locale('zh-cn');
-ReactDOM.render(<LocaleProvider locale={zhCN}><App /></LocaleProvider>, document.getElementById('root'));
+
+ReactDOM.render(<App />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
