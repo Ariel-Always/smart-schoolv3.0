10fbd33edc096e6b0b0c45100fd9d595910ae479
Ariel-Always <zhangyangllx@163.com>
2019-08-14 14:31:14 +0800

    取消console.log上传login界面（静态）

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -125,8 +125,6 @@ module.exports = function(webpackEnv) {
          v].filter(Boolean);
   }); //根据所有的入口index.js（除了common目录下）来设置入口entry
 
-  console.log(entrys);
-
   const  modulehtmls = paths.htmlArray.map(function (v) { //设置其他模块的入口index.html
       let arr = v.split('/');
       return new HtmlWebpackPlugin({
