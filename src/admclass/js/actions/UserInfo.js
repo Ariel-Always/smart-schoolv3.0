e84f49fc359148649ad81a31cc496b558570f2d0
Ariel-Always <zhangyangllx@163.com>
2019-08-15 11:08:09 +0800

    合并冲突

--- src/admclass/js/actions/UserInfo.js
+++ src/admclass/js/actions/UserInfo.js
@@ -1,17 +1,13 @@
-import {postData} from "../../../common/js/fetch";
+import {postData,getData} from "../../../common/js/fetch";
 import uiStateUpdate from './UIState';
 
 import CONFIG from '../../../common/js/config';
 const GET_USER_INFO = "GET_USER_INFO";
 const  getUserInfo = (url) => {
     return (dispatch)=>{
-        postData(CONFIG.proxy+url).then(res=>{
+        getData(CONFIG.proxy+url).then(res=>res.json()).then(res=> {
             console.log(res);
         });
-        /*fetch(CONFIG.proxy+url).then(res=>res.json()).then((res)=>{
-            dispatch({type:GET_USER_INFO,userInfo:res.Data});
-            dispatch({type:uiStateUpdate.APPLOADINGCLOSE});
-        })*/
     }
 };
 
