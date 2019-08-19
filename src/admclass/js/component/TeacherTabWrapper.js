2e7835272558169b07cfdc26908e2a12bb13b2df
Ariel-Always <zhangyangllx@163.com>
2019-08-19 23:40:09 +0800

    2019-08-19

--- src/admclass/js/component/TeacherTabWrapper.js
+++ src/admclass/js/component/TeacherTabWrapper.js
@@ -1,4 +1,7 @@
 import React,{Component} from 'react';
+
+import HeaderImg from '../../images/teacher-head-img.png';
+
 class TeacherTabWrapper extends Component{
     render() {
 
@@ -10,14 +13,37 @@ class TeacherTabWrapper extends Component{
 
                 <div className="admclass-teacher-ganger">
 
+                    <div className="admclass-teacher-photo" style={{backgroundImage:`url(${HeaderImg})`}}></div>
+
+                    <div className="admclass-teacher-info">
+
+                        <div className="admclass-teacher-name">{Teachers.Ganger&&Teachers.Ganger.UserName}</div>
+
+                        <div className="admclass-teacher-id">{Teachers.Ganger&&Teachers.Ganger.UserID}</div>
+
+                    </div>
+
                 </div>
 
                 {
                     Teachers.List&&Teachers.List.map((item,key) => {
+
                        return <div className="admclass-teacher-item">
 
+                                   <div className="admclass-teacher-photo" style={{backgroundImage:`url(${HeaderImg})`}}></div>
+
+                                   <div className="admclass-teacher-info">
+
+                                       <div className="admclass-teacher-name">{Teachers.Ganger&&Teachers.Ganger.UserName}</div>
+
+                                       <div className="admclass-teacher-id">{Teachers.Ganger&&Teachers.Ganger.UserID}</div>
+
+                                   </div>
+
                                </div>
+
                     })
+
                 }
 
             </div>
