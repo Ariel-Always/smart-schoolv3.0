d8c43776dd1df55bc0ef816440d3a699c4e70f4b
zhuzesen <m15626248624@163.com>
2019-08-22 22:45:44 +0800

    8/22；zhuzesen

--- src/admArchives/js/containers/App.js
+++ src/admArchives/js/containers/App.js
@@ -24,39 +24,7 @@ class App extends Component {
         super(props);
         const { dispatch } = props;
         this.state = {
-            MenuParams: {
-                MenuBox: {
-                    display: true,
-                    width: 240,
-                    MenuBoxTopPic: 'pic1'
-                },
-                children: [{
-                    key: 'All',
-                    title: '用户档案总览',
-                    icon: 'menu10',
-                    onTitleClick: this.handleClick,
-                    active: true,
-                    selected: true
-                },
-                {
-                    key: 'Student',
-                    title: '学生档案',
-                    icon: 'menu39',
-                    onTitleClick: this.handleClick,
-                },
-                {
-                    key: 'Teacher',
-                    title: '教师档案',
-                    icon: 'menu33',
-                    onTitleClick: this.handleClick,
-                },
-                {
-                    key: 'Leader',
-                    title: '领导档案',
-                    icon: 'menu35',
-                    onTitleClick: this.handleClick,
-                }]
-            }
+
         }
         let route = history.location.pathname;
         //判断token是否存在
@@ -90,9 +58,11 @@ class App extends Component {
             // 获取接口数据
             this.requestData(route)
 
-            $('.frame_leftmenu_mainitem').removeClass('selected active');
-            $('.frame_leftmenu_mainitem').children('*').removeClass('active');
-            this.handleMenu()
+
+            if (history.location.pathname === '/' || history.location.pathname === '/UserArchives') {
+                history.push('/UserArchives/All')
+                console.log(this.state)
+            }
         })
     }
     componentWillUpdate() {
@@ -119,7 +89,8 @@ class App extends Component {
     // 请求每个组件主要渲染的数据
     requestData = (route) => {
         const { dispatch } = this.props;
-        let handleRoute = route.split('/')[2];
+        let pathArr = route.split('/');
+        let handleRoute = pathArr[2];
         if (route === '/' || route.split('/')[1] === 'UserArchives') {
             dispatch(actions.UpDataState.getAllUserPreview('/ArchivesAll'));
             dispatch({ type: actions.UpUIState.APP_LOADING_CLOSE });
@@ -129,40 +100,29 @@ class App extends Component {
                 if (route === '/UserArchives/Student') {
                     console.log('sds')
                     if (!this.props.DataState.GradeClassMsg.returnData)
-                        dispatch(actions.UpDataState.getGradeClassMsg('/Archives' + handleRoute + '_DropDownMenu'));
-                    dispatch(actions.UpDataState.getGradeStudentPreview('/Archives' + handleRoute + '?SchoolID=schoolID&GradeID=gradeID&ClassID=ClassID&PageIndex=0&PageSize=10&SortFiled=UserID&SortType=ASC'));
+                        dispatch(actions.UpDataState.getGradeClassMsg('/ArchivesStudent_DropDownMenu'));
+                    dispatch(actions.UpDataState.getGradeStudentPreview('/ArchivesStudent?SchoolID=schoolID&GradeID=gradeID&ClassID=ClassID&PageIndex=0&PageSize=10&SortFiled=UserID&SortType=ASC'));
                 }
             }
 
         }
-        else {
-
+        else if (route.split('/')[1] === 'RegisterExamine') {
+            //dispatch(actions.UpDataState.getAllUserPreview('/RegisterExamine'));
+            dispatch({ type: actions.UpUIState.APP_LOADING_CLOSE });
+            if (!this.props.DataState.GradeClassMsg.returnData)
+                dispatch(actions.UpDataState.getGradeClassMsg('/ArchivesStudent_DropDownMenu'));
         }
 
 
     }
     //操作左侧菜单，响应路由变化
     handleMenu = () => {
-        if (history.location.pathname === '/'||history.location.pathname === '/UserArchives') {
+        if (history.location.pathname === '/' || history.location.pathname === '/UserArchives') {
             history.push('/UserArchives/All')
             console.log(this.state)
         }
-        let path = history.location.pathname.substr(1);
 
-        let param = this.state.MenuParams;
-        let len = param.children.length;
 
-        for (let i = 0; i < len; i++) {
-            param.children[i]['active'] = false;
-            param.children[i]['selected'] = false;
-            if (path === param.children[i].key) {
-                param.children[i]['active'] = true;
-                param.children[i]['selected'] = true;
-                this.setState({
-                    MenuParams: param
-                })
-            }
-        }
     }
     //左侧菜单每项的点击事件
     handleClick = (key) => {
