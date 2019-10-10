ca43aa91d4d1812381c53af1ff2668cfc92be554
zhuzesen <m15626248624@163.com>
2019-10-10 14:53:16 +0800

    zhuzesen

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,23 +1,4 @@
 import logo from '../images/frame/logo.png';
-<<<<<<< HEAD
-export default {
-  name:"中小学学科一体化教育云",
-  logo:logo,
-  footer:"蓝鸽科技 版权所有",
-  SubjectProxy:'http://192.168.129.242:8086/Subject/api',
-  CourseClassProxy:'http://192.168.2.9:8082/CourseClass/api',
-  UserAccountProxy:'http://192.168.2.248:8075/UserMgr/UserAccount',
-  TeachingSolutionProxy:'http://192.168.2.114:8090/SubjectResMgr/TeachingSolutionMgr/Teacher',
-  AdmClassProxy:"http://192.168.2.248:8075",
-  DeskTopProxy:"http://192.168.2.202:7300/mock/5d7726e0ed0ccd1564c8df05/webCloudDev",
-  Xproxy:'http://192.168.2.248:8075/UserMgr/UserInfoMgr',
-  PowerProxy:'http://192.168.2.248:8075/UserMgr/PowerMgr',
-  UserInfoProxy:'http://192.168.2.248:8075/UserMgr/UserInfoMgr',
-  proxy:"http://192.168.2.202:7300/mock/5d7726e0ed0ccd1564c8df05/webCloudDev",
-  BasicProxy:'http://localhost:3000',
-  LoginProxy:'http://192.168.129.2:10102'
-};
-=======
 
 let config = {};
 
@@ -71,4 +52,3 @@ if (process.env.NODE_ENV === 'production'){
 
 
 export default config;
->>>>>>> 5a24c9ee80e7b001e1b77749346c233b907a3b5a
