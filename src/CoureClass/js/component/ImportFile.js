6f73d68244769b37eeb91056f1dfb15eea367966
zhuzesen <m15626248624@163.com>
2019-10-23 20:36:34 +0800

    10/23

--- src/CoureClass/js/component/ImportFile.js
+++ src/CoureClass/js/component/ImportFile.js
@@ -37,9 +37,10 @@ class ImportFile extends React.Component {
         const { DataState, dispatch } = this.props;
     }
     componentDidMount() {
-        // $.get(CONFIG.UserInfoProxy+'/Import.aspx?Token=0111&UserType=Student',function(data){
-        //     $('#content-box').html(data)
-        // })
+        $.get('/UserMgr/TeachInfoMgr/Import.aspx?SchoolID='+this.state.userMsg.SchoolID+'&Type=courseclass&UserID='+this.state.userMsg.UserID+'&UserName='+this.state.userMsg.UserName+'&Token='+ sessionStorage.getItem('token'),function(data){
+            console.log(data)
+            $('#content-box').html(data)
+        })
 
         // $.ajax({
         //     url:CONFIG.UserInfoProxy+'/Import.aspx?Token=0111&UserType=Student',
@@ -53,26 +54,27 @@ class ImportFile extends React.Component {
         //     console.log(data)
         //     $('#content-box').html(data)
         // })
-        let route = history.location.pathname.split('/');
+        // let route = history.location.pathname.split('/');
 
-        fetch(CONFIG.UserInfoProxy + '/UserMgr/TeachInfoMgr/Import.aspx?SchoolID='+this.state.userMsg.SchoolID+'&Type=courseclass&UserID='+this.state.userMsgUserID+'&UserName='+this.state.userMsg.UserName+'&Token='+ sessionStorage.getItem('token'), {
-            method: 'get',//*post、get、put、delete，此项为请求方法相关的配置 
-            mode: 'cors',//no-cors(跨域模式但服务器端不支持cors),*cors(跨域模式，需要服务器通过Access-control-Allow-Origin来
-            //允许指定的源进行跨域),same-origin(同源)
-            cache: 'no-cache',//*no-cache,default,reload,force-cache,only-ifcached,此项为缓存相关配置
-            credentials: 'omit',//*include(携带cookie)、same-origin(cookie同源携带)、omit(不携带)
+        
+        // ('/UserMgr/TeachInfoMgr/Import.aspx?SchoolID='+this.state.userMsg.SchoolID+'&Type=courseclass&UserID='+this.state.userMsg.UserID+'&UserName='+this.state.userMsg.UserName+'&Token='+ sessionStorage.getItem('token'), {
+        //     method: 'get',//*post、get、put、delete，此项为请求方法相关的配置 
+        //     mode: 'cors',//no-cors(跨域模式但服务器端不支持cors),*cors(跨域模式，需要服务器通过Access-control-Allow-Origin来
+        //     //允许指定的源进行跨域),same-origin(同源)
+        //     cache: 'no-cache',//*no-cache,default,reload,force-cache,only-ifcached,此项为缓存相关配置
+        //     credentials: 'omit',//*include(携带cookie)、same-origin(cookie同源携带)、omit(不携带)
 
-            headers: {
-                'Accept': 'application/json, text/plain, text/html,*/*',//请求头，代表的、发送端（客户端）希望接收的数据类型
-                'Content-Type': 'application/x-www-form-urlencoded',//实体头，代表发送端（客户端|服务器）发送的实体数据的数据类型
-            },
-            redirect: 'follow',//manual、*follow(自动重定向)、error，此项为重定向的相关配置
-            // referrer: 'no-referrer',//该首部字段会告知服务器请求的原始资源的URI
+        //     headers: {
+        //         'Accept': 'application/json, text/plain, text/html,*/*',//请求头，代表的、发送端（客户端）希望接收的数据类型
+        //         'Content-Type': 'application/x-www-form-urlencoded',//实体头，代表发送端（客户端|服务器）发送的实体数据的数据类型
+        //     },
+        //     redirect: 'follow',//manual、*follow(自动重定向)、error，此项为重定向的相关配置
+        //     // referrer: 'no-referrer',//该首部字段会告知服务器请求的原始资源的URI
 
-        }).then(data => {
-            console.log(data)
-            $('#content-box').html(data)
-        })
+        // }).then(data => {
+        //     console.log(data)
+        //     $('#content-box').html(data)
+        // })
     }
 
     //点击tab
