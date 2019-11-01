3effd2694cf70f25e3ef4065d43d7d56f3188396
zhuzesen <m15626248624@163.com>
2019-11-01 17:26:38 +0800

    11/1

--- src/index/js/reducers/Teacher/TeacherCustomModalShow.js
+++ src/index/js/reducers/Teacher/TeacherCustomModalShow.js
@@ -1,12 +1,16 @@
 import HeaderActions from '../../actions/Teacher/HeaderActions';
-const TeacherCustomModalShow = (state={Show:false,key:'tool'},actions) => {
+const TeacherCustomModalShow = (state = { Show: false, key: 'tool', AddCustomModalShow: false }, actions) => {
     switch (actions.type) {
         case HeaderActions.TEACHER_CUSTOM_MODAL_OPEN:
-            return Object.assign({},state,{Show:true,key:actions.key});
+            return Object.assign({}, state, { Show: true, key: actions.key });
         case HeaderActions.TEACHER_CUSTOM_MODAL_CLOSE:
-            return Object.assign({},state,{Show:false,key:actions.key});            
+            return Object.assign({}, state, { Show: false, key: actions.key });
+        case HeaderActions.ADD_CUSTOM_MODAL_OPEN:
+            return Object.assign({}, state, { AddCustomModalShow: true });
+        case HeaderActions.ADD_CUSTOM_MODAL_CLOSE:
+            return Object.assign({}, state, { AddCustomModalShow: false });
         default:
             return state;
     }
 };
-export default  TeacherCustomModalShow;
+export default TeacherCustomModalShow;
